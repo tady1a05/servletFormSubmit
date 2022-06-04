4e65575c67c8800c17c453a432913b288d8c2f54
toby1a05 <32935616+toby1a05@users.noreply.github.com>
2022-06-04 05:17:31 -0700

    first commit

---
+++ src/main/java/com/servletFormSubmit/test1.java
@@ -0,0 +1,48 @@
+package com.servletFormSubmit;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class test1
+ */
+@WebServlet("/test1")
+public class test1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public test1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		out.println("<h1>"+request.getParameter("firstName") + request.getParameter("lastName")+"</h1>");
+		out.println("</html></body>");
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/studentForm.jsp
@@ -0,0 +1,12 @@
+<html>
+<head></head>
+<body>
+	<form method="get" action="test1">
+		<label for="firstName">student first name:</label>
+		<input name="firstName" type="text"/>
+		<label for="lastName">student last name:</label>
+		<input name="lastName" type="text"/>
+		<input type="submit"/>
+	</form>
+</body>
+</html>
